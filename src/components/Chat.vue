4ef8cc25950832b44288069446cae86050962014
othaderek <otha.hernandez@gmail.com>
2021-02-26 18:41:46 -0500

    Made changes.

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -8,18 +8,19 @@
         mounted () {
             console.log('mounted')
             this.$socket.emit('mounted', 'Message from client')
+            this.$socket.emit('join-room', Object.keys(this), 11)
             },
         sockets : {
-        test(data) {
-        console.log('test triggered', data)
-        },
-        connect() {
-        console.log('connected')
-        },
-        disconnect() {
+            test(data) {
+                console.log('test triggered: ', data)
+                },
+            connect() {
+                console.log('connected')
+                },
+            disconnect() {
                 console.log("server disconnected");
+                }
             }
-    }
 
     }
 </script>
